5e996795530bb99f75f9f056bd92353aa5aa92c6
roshan kc <47151288+roshankc123@users.noreply.github.com>
2023-06-09 11:55:15 +0545

    websocket basic using php and raw connection  added

---
+++ websockets/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTTP-Long-polling</title>
+    <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
+    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+</head>
+<body onload="">
+    <div id="main"></div>
+</body>
+<script type='text/babel'>
+
+    const container = document.getElementById('main');
+    var doc;
+    var root;
+        function RenderElement(props){
+            return <p>{props.message}</p>
+        }
+        function Load(message){
+            var doc = document.createElement('span')
+            var root = ReactDOM.createRoot(doc)
+            root.render(<RenderElement message={message}/>)
+            container.appendChild(doc)
+        }
+
+        if(typeof(EventSource) != 'undefined'){
+            var source = new EventSource('/server.php');
+            source.onopen = (event) => {
+                console.log('opened')
+            }
+            source.addEventListener('hello', (event) => {
+                console.log('hello came')
+            })
+            source.onmessage = function(event){
+                console.log(event)
+                Load(event.data)
+            }
+            
+        }else {
+            document.getElementById("main").innerHTML = "Sorry, your browser does not support server-sent events...";
+        }
+    </script>
+
+</html>
---
+++ websockets/server.php
@@ -0,0 +1,138 @@
+<?php
+
+    //for debugging set this to 1
+    ini_set('error_reporting', 0);
+    
+    
+    // Set time limit to indefinite execution(no timeout)
+    
+    set_time_limit (0);
+    
+    // Set the ip and port we will listen on
+    
+    $address = '127.0.0.1';
+    
+    $port = 6901;
+    
+    // Create a TCP Stream socket
+    
+    $sock = socket_create(AF_INET, SOCK_STREAM, 0);
+    
+    // Bind the socket to an address/port
+    
+    socket_bind($sock, $address, $port) or die('Could not bind to address'.PHP_EOL);
+
+    printf('socket created'.PHP_EOL);
+    
+    
+    
+    // Start listening for connections
+    
+    socket_listen($sock);
+    
+    
+    
+    // Non block socket type
+    
+    socket_set_nonblock($sock);
+    
+    
+    
+
+    $client = [];  // contain a list of associative array {'id', 'connection handler'}
+
+    // Loop continuously
+    while (true)
+    
+    {
+    
+        if ($newsock = @socket_accept($sock))
+    
+        {
+
+            $conn['id'] = count($client);
+
+            $conn['conn'] = $newsock;
+
+            array_push($client, $conn);
+
+            foreach ($client as $key => $value) {
+
+                printf('new client '.$value['id'].' connected'.PHP_EOL);
+
+                send($conn['id'], $value['conn'], 'new client connected '.$conn['id'].PHP_EOL);
+
+            }
+        }
+        
+    
+        if (count($client))
+        
+        {
+    
+            foreach ($client as $key => $value)
+    
+            {
+    
+                ///send empty string to close the connection, eg. echo '' | nc 127.0.0.1 6091
+                if (@socket_recv($value['conn'], $string, 1024, MSG_DONTWAIT) === 0)
+    
+                {
+                    $message = $value['id'].' closed the connection'.PHP_EOL;
+
+                    foreach ($client as $k => $v) {
+
+                        send($value['id'], $v['conn'], $message);
+
+                    }
+                    
+                    socket_close($sock);
+
+                    printf($message);
+
+                    exit;
+                }
+    
+                else
+    
+                {
+    
+                    if ($string)
+    
+                    {
+
+                        foreach ($client as $k => $v) {
+
+                            send($value['id'], $v['conn'], $string);
+
+                        }
+
+                        printf($value['id'].' to all : '.$string);
+                        
+                    }
+    
+                }
+    
+            }
+    
+        }
+    
+        sleep(1);   ///save the power
+    
+    }
+    
+    
+    
+    // Close the master sockets
+    socket_close($sock);
+
+    ///send the message to specified socket
+    function send($sender, $receiver, $message){
+
+        $message_f = "$sender> $message";
+
+        socket_write($receiver, $message_f, strlen($message_f)).chr(0);
+
+    }
+    
+?>
